5b9b0d1993e640c12dc695c8226f45e1ad06ef68
Fahad <fahadkshehri@gmail.com>
2019-05-08 23:51:45 -0700

    Implemented addNode and findKey in Bucket class

--- src/com/gd/practice/ds/MyHashMap.java
+++ src/com/gd/practice/ds/MyHashMap.java
@@ -11,6 +11,7 @@ class HashMapImpl {
 
     }
 
+
     public void put(String key, String value) {
         if (key == null) {
             throw new RuntimeException("null key is not supported.");
@@ -44,18 +45,72 @@ class HashMapImpl {
 }
 
 class Bucket {
+    //LinkedList<Node> list = new LinkedList<Node>();
+    Node firstNode;
 
+
+    public void addNode(Node node){
+        //Nothing is there yet
+        if(firstNode == null){
+            firstNode = node;
+        }
+
+        Node currentNode = firstNode;
+        //case exists, and case not exists and the same time
+        while (currentNode != null){
+            if(currentNode.hash == node.hash){
+                currentNode.value = node.value;
+                break;
+            }
+            if(currentNode.next == null){
+                currentNode.next = node;
+                break;
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
+    /**
+ -    * Find if a given key's hash exists in the hashtable
+     * @param hash
+     * @return the node where the hash exits within the table
+     */
+    public Node findKey(int hash){
+        Node current = firstNode;
+
+        while (current != null){
+            if(current.hash == hash){
+                //returning a node because this makes the most since when we want to use it
+                // in the Hashmap class methods
+                break;
+            }
+            current = current.next;
+
+        }
+        return current;
+    }
 }
 
 class Node {
-    private String key;
-    private String value;
-    private int hash;
+    // how do we prevent them from being hacked, or follow safe practice
+    // because I know that an object's state show always be private for security reasons
+    String key;
+    String value;
+    int hash;
+    Node next;
 
+    Node(){
+        this.key = key;
+        this.value = value;
+        this.hash = hash;
+        this.next = next;
+    }
     Node(String key, String value, int hash) {
         this.hash = hash;
         this.key = key;
         this.value = value;
+        this.next = next;
+
     }
 
     @Override
