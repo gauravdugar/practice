f2947ddb0a6ec11ffb14c499e4a9be5eefd26f95
gauravdugar <gaurav.dugar1506@gmail.com>
2019-05-09 00:03:56 -0700

    Basic implementation of hashmap

--- src/com/gd/practice/ds/test/MyHashMap.java
+++ src/com/gd/practice/ds/test/MyHashMap.java
@@ -4,30 +4,43 @@ import java.util.ArrayList;
 
 class HashMapImpl {
 
-    private int BUCKET_COUNT = 16;
+    private int BUCKET_COUNT = 4;
 
     private ArrayList<Bucket> bucketList;
 
     HashMapImpl() {
         bucketList = new ArrayList<>();
+
+        int bucketIndex = 0;
+        while (bucketIndex < BUCKET_COUNT) {
+            bucketList.add(new Bucket(bucketIndex));
+            bucketIndex++;
+        }
     }
 
     public void put(String key, String value) {
         if (key == null) {
             throw new RuntimeException("null key is not supported.");
         }
+        Node n = new Node(key, value, key.hashCode());
+        Bucket b = bucketList.get(getBucketIndexByKey(key));
+        b.addNode(n);
     }
 
     public String get(String key) {
-        return null;
+        Bucket b = bucketList.get(getBucketIndexByKey(key));
+        Node n = b.findNode(key.hashCode());
+        return n == null ? null : n.value;
     }
 
     public boolean contains(String key) {
-        return true;
+        Bucket b = bucketList.get(getBucketIndexByKey(key));
+        return b.findNode(key.hashCode()) != null;
     }
 
     public void remove(String key) {
-
+        Bucket b = bucketList.get(getBucketIndexByKey(key));
+        b.removeNode(key);
     }
 
     private void rearrange() {
@@ -41,14 +54,29 @@ class HashMapImpl {
 
     public void printMap() {
         System.out.println("Bucket Count: " + BUCKET_COUNT);
+
+        for (Bucket b : bucketList) {
+            System.out.println("Bucket " + b.bucketIndex);
+            Node currentNode = b.firstNode;
+            while (currentNode != null) {
+                System.out.println(currentNode);
+                currentNode = currentNode.nextNode;
+            }
+            System.out.println();
+        }
     }
 }
 
 class Bucket {
 
+    int bucketIndex;
     Node firstNode;
 
-    public Node findNode(int hash) {
+    Bucket(int bucketIndex) {
+        this.bucketIndex = bucketIndex;
+    }
+
+    Node findNode(int hash) {
         Node currentNode = firstNode;
         while (currentNode != null) {
             if (currentNode.hash == hash) {
@@ -57,6 +85,46 @@ class Bucket {
             currentNode = currentNode.nextNode;
         }
         return currentNode;
+    }
+
+    void addNode(Node newNode) {
+
+        if (firstNode == null) {
+            firstNode = newNode;
+        } else {
+            Node currentNode = firstNode;
+            while (true) {
+                if (currentNode.hash == newNode.hash && currentNode.key.equals(newNode.key)) {
+                    currentNode.value = newNode.value;
+                    return;
+                }
+                if (currentNode.nextNode == null) {
+                     currentNode.nextNode = newNode;
+                     return;
+                }
+                currentNode = currentNode.nextNode;
+            }
+        }
+    }
+
+    void removeNode(String key) {
+
+        if (firstNode == null) {
+            throw new RuntimeException("Nothing to remove");
+        } else if (firstNode.hash == key.hashCode() && firstNode.key.equals(key)) {
+            firstNode = null;
+        } else {
+            Node currentNode = firstNode;
+            Node nextNode = currentNode.nextNode;
+            while (nextNode != null) {
+                if (nextNode.hash == key.hashCode() && nextNode.key.equals(key)) {
+                    currentNode.nextNode = nextNode.nextNode;
+                    return;
+                }
+                currentNode = nextNode;
+                nextNode = nextNode.nextNode;
+            }
+        }
     }
 }
 
@@ -80,6 +148,19 @@ class Node {
 
 public class MyHashMap {
     public static void main(String args[]) {
-        System.out.println("This is my HashMap implementation");
+        System.out.println("This is my HashMap implementation\n");
+        HashMapImpl map = new HashMapImpl();
+        map.put("key1", "v1");
+        map.put("key2", "v2");
+        map.put("key3", "v3");
+        map.put("key4", "v4");
+        map.printMap();
+
+        System.out.println("Is k3 in the map: " + map.contains("k3"));
+        map.remove("k3");
+        System.out.println("Is k3 in the map: " + map.contains("k3"));
+
+        map.put("key3", "v3");
+        map.printMap();
     }
 }
