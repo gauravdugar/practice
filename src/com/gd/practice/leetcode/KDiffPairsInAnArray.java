718d92079d924c29742b259b384189e7313e22f4
Gaurav Dugar <gaurav.dugar1506@gmail.com>
2020-02-03 22:48:40 -0800

    Adding some Twitter easy problems.

---
+++ src/com/gd/practice/leetcode/KDiffPairsInAnArray.java
@@ -0,0 +1,14 @@
+package com.gd.practice.leetcode;
+
+/**
+ * 532. K-diff Pairs in an Array (INCOMPLETE SOLUTION)
+ * Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array.
+ * Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their
+ * absolute difference is k.
+ * https://leetcode.com/problems/k-diff-pairs-in-an-array/
+ */
+public class KDiffPairsInAnArray {
+    public int findPairs(int[] nums, int k) {
+        return 0;
+    }
+}
---
+++ src/com/gd/practice/leetcode/TwoSum.java
@@ -0,0 +1,13 @@
+package com.gd.practice.leetcode;
+
+/**
+ * 1. Two Sum (INCOMPLETE SOLUTION)
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * https://leetcode.com/problems/two-sum/
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
